ca6ed7b6f5f96822f8a392482fb8814c3d6242a3
MarkDigital <markdigital@protonmail.com>
2017-07-05 10:56:10 +1000

    Create Appointment Getter

---
+++ getAppointments.java
@@ -0,0 +1,18 @@
+public class getAppointments {
+
+
+  /** 
+  *This Method Gets the Appointments stored in CSV file.
+  *User Login determines which appointments are pulled.
+  */
+  public void getAppointments {
+  
+  }
+  
+   /** 
+  *This method pulls permissions from the employee database (Probably CSV)
+  */
+  public void getPermissions {
+  
+  }
+}
