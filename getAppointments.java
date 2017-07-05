2b4ad2b958ee91a04ecfd5faf237e588a50153f2
MarkDigital <markdigital@protonmail.com>
2017-07-05 11:29:02 +1000

    Update getAppointments.java

--- getAppointments.java
+++ getAppointments.java
@@ -1,18 +1,28 @@
 public class getAppointments {
+  
+  ArrayList<String> permissions = new ArrayList<String>();
+  ArrayList<String> appointments = new ArrayList<String>();
 
 
   /** 
   *This Method Gets the Appointments stored in CSV file.
   *User Login determines which appointments are pulled.
   */
-  public void getAppointments {
-  
+  public void getAppointments (String userName) {
+    
+    getPermissions(userName);
+    
+    if(permissions.get(1) == "ADMIN"){
+    //PULL ALL APPOINTMENTS
+    } else {
+    //PULL ONLY THE APPOINTMENTS SET BY USER.
+    }
   }
   
-   /** 
+  /** 
   *This method pulls permissions from the employee database (Probably CSV)
   */
-  public void getPermissions {
-  
+  public void getPermissions (String userName){
+    //Get Permissions based on userName.
   }
 }
